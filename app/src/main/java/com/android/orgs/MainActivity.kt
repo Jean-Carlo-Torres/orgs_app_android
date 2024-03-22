c588b3e76227de297269578f4eed952d9deb18b2
Jean Carlo <jeansanto2002@gmail.com>
2024-03-22 16:27:37 -0300

    Adicionando unidades de medidas no layout

--- app/src/main/java/com/android/orgs/MainActivity.kt
+++ app/src/main/java/com/android/orgs/MainActivity.kt
@@ -2,11 +2,11 @@ package com.android.orgs
 
 import android.app.Activity
 import android.os.Bundle
-import android.widget.Toast
+import android.widget.TextView
 
 class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Toast.makeText(this, "Bem-vindo(a) ao Orgs!", Toast.LENGTH_SHORT).show()
+        setContentView(R.layout.activity_main)
     }
 }

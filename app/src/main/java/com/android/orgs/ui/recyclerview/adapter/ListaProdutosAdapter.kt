4c36d8f8c39c6dd1a05f2613474ce162ab0b0fa3
Jean Carlo <jeansanto2002@gmail.com>
2024-03-26 10:39:26 -0300

    Personalizando produto com Card

--- app/src/main/java/com/android/orgs/ui/recyclerview/adapter/ListaProdutosAdapter.kt
+++ app/src/main/java/com/android/orgs/ui/recyclerview/adapter/ListaProdutosAdapter.kt
@@ -1,11 +1,14 @@
 package com.android.orgs.ui.recyclerview.adapter
 
 import android.content.Context
+import android.icu.text.NumberFormat
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.android.orgs.databinding.ProdutoItemBinding
 import com.android.orgs.model.Produto
+import java.math.BigDecimal
+import java.util.*
 
 class ListaProdutosAdapter(
     private val context: Context,
@@ -23,7 +26,14 @@ class ListaProdutosAdapter(
             val descricao = binding.produtoItemDescricao
             descricao.text = produto.descricao
             val valor = binding.produtoItemValor
-            valor.text = produto.valor.toPlainString()
+            val valorEmMoeda: String = formataParaMoedaBrasileira(produto.valor)
+            valor.text = valorEmMoeda
+        }
+
+        private fun formataParaMoedaBrasileira(valor: BigDecimal): String {
+            val formatador: NumberFormat = NumberFormat
+                .getCurrencyInstance(Locale("pt", "br"))
+            return formatador.format(valor)
         }
     }
 

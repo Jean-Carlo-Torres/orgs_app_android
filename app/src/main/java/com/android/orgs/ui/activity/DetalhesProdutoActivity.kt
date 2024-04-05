991c0ec3a01c9ff3eb033546dd86f667a439a7f6
Jean Carlo <jeansanto2002@gmail.com>
2024-04-05 09:00:35 -0300

    Implementando listener dos menus

--- app/src/main/java/com/android/orgs/ui/activity/DetalhesProdutoActivity.kt
+++ app/src/main/java/com/android/orgs/ui/activity/DetalhesProdutoActivity.kt
@@ -1,11 +1,17 @@
 package com.android.orgs.ui.activity
 
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import com.android.orgs.R
 import com.android.orgs.databinding.ActivityDetalhesProdutoBinding
 import com.android.orgs.extensions.formatarParaMoedaBrasileira
 import com.android.orgs.extensions.tentaCarregarImagem
 import com.android.orgs.model.Produto
+
+private const val TAG = "DetalhesProdutoActivity"
 
 class DetalhesProdutoActivity : AppCompatActivity() {
 
@@ -18,6 +24,24 @@ class DetalhesProdutoActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         tentaCarregarProduto()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_detalhes_produto, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_detalhes_produto_remover -> {
+                Log.i(TAG, "onOptionsItemSelected: remover")
+            }
+
+            R.id.menu_detalhes_produto_editar -> {
+                Log.i(TAG, "onOptionsItemSelected: editar")
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     private fun tentaCarregarProduto() {

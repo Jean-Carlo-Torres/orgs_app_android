18398798a8cee0dcb730375f3888044b507e09c1
Jean Carlo <jeansanto2002@gmail.com>
2024-04-07 09:07:51 -0300

    Criando menu para ordenar produtos na lista

--- app/src/main/java/com/android/orgs/database/dao/ProdutoDao.kt
+++ app/src/main/java/com/android/orgs/database/dao/ProdutoDao.kt
@@ -17,4 +17,22 @@ interface ProdutoDao {
 
     @Query("SELECT * FROM Produto WHERE id = :id")
     fun buscaPorId(id: Long): Produto?
+
+    @Query("SELECT * FROM Produto ORDER BY nome DESC")
+    fun ordenarProdutosPorNomeDecrescente(): List<Produto>
+
+    @Query("SELECT * FROM Produto ORDER BY nome ASC")
+    fun ordenarProdutosPorNomeCrescente(): List<Produto>
+
+    @Query("SELECT * FROM Produto ORDER BY descricao DESC")
+    fun ordenarProdutosPorDescricaoDecrescente(): List<Produto>
+
+    @Query("SELECT * FROM Produto ORDER BY descricao ASC")
+    fun ordenarProdutosPorDescricaoCrescente(): List<Produto>
+
+    @Query("SELECT * FROM Produto ORDER BY valor DESC")
+    fun ordenarProdutosPorValorDecrescente(): List<Produto>
+
+    @Query("SELECT * FROM Produto ORDER BY valor ASC")
+    fun ordenarProdutosPorValorCrescente(): List<Produto>
 }
--- app/src/main/java/com/android/orgs/ui/activity/ListaProdutosActivity.kt
+++ app/src/main/java/com/android/orgs/ui/activity/ListaProdutosActivity.kt
@@ -2,7 +2,10 @@ package com.android.orgs.ui.activity
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import com.android.orgs.R
 import com.android.orgs.database.OrgsAppDatabase
 import com.android.orgs.databinding.ActivityListaProdutosBinding
 import com.android.orgs.model.Produto
@@ -17,6 +20,9 @@ class ListaProdutosActivity : AppCompatActivity() {
     private val binding by lazy {
         ActivityListaProdutosBinding.inflate(layoutInflater)
     }
+    private val produtosDao by lazy {
+        OrgsAppDatabase.instancia(this).produtoDao()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,9 +33,36 @@ class ListaProdutosActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
-        val db = OrgsAppDatabase.instancia(this)
-        val produtosDao = db.produtoDao()
         adapter.atualiza(produtosDao.buscaTodos())
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_ordenacao_lista_produtos, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val produtosOrdenado: List<Produto>? = when (item.itemId) {
+            R.id.menu_lista_produtos_ordenar_nome_crescente ->
+                produtosDao.ordenarProdutosPorNomeCrescente()
+            R.id.menu_lista_produtos_ordenar_nome_decrescente ->
+                produtosDao.ordenarProdutosPorNomeDecrescente()
+            R.id.menu_lista_produtos_ordenar_descricao_crescente ->
+                produtosDao.ordenarProdutosPorDescricaoCrescente()
+            R.id.menu_lista_produtos_ordenar_descricao_decrescente ->
+                produtosDao.ordenarProdutosPorDescricaoDecrescente()
+            R.id.menu_lista_produtos_ordenar_valor_crescente ->
+                produtosDao.ordenarProdutosPorValorCrescente()
+            R.id.menu_lista_produtos_ordenar_valor_decrescente ->
+                produtosDao.ordenarProdutosPorValorDecrescente()
+            R.id.menu_lista_produtos_ordenar_sem_ordem ->
+                produtosDao.buscaTodos()
+            else -> null
+        }
+        produtosOrdenado?.let {
+            adapter.atualiza(it)
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     private fun configuraFab() {
